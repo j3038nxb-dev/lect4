3bd94a76d3d4d0677e4969d3c3acb2e0e5876edd
Su_YuuuuuuX <j3038nxb@sit.ac.jp>
2025-11-26 14:32:28 +0900

    add lisence

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Su_YuuuuuuX
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}"=14 ] || ng ${LINENO}
+
+[ "${out}"=0 ] && echo OK
+
+exit $res
